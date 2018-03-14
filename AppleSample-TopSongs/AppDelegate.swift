85d391a6bf3a6c11c2f3dbea954bf80f14cb80ab
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-03-13 22:13:42 -0700

    add code to delete the sqlite file to reset the database

--- AppleSample-TopSongs/AppDelegate.swift
+++ AppleSample-TopSongs/AppDelegate.swift
@@ -13,20 +13,26 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
+
     // Properties for the importer and its background processing queue.
     var importer: iTunesJSONImporter?
-    
+
     // Properties for the Core Data stack.
     var managedObjectContext: NSManagedObjectContext?
     var persistentStoreCoordinator: NSPersistentStoreCoordinator?
-    var persistentStorePath: String?
-    
+    var persistentStorePath: URL? = {
+        if let applicationSupportDirectory = try? FileManager.default.url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true) {
+            print("applicationSupportDirectory exists")
+            return applicationSupportDirectory.appendingPathComponent("TopSongs.sqlite")
+        }
+        return nil
+    }()
+
     // The number of songs to be retrieved from the RSS feed.
     var importSize = 200
     lazy var iTunesURL: URL = {
         let urlString = "https://rss.itunes.apple.com/api/v1/us/itunes-music/top-songs/all/\(importSize)/explicit.json"
-        
+
         return URL(string: urlString)!
     }()
 
@@ -35,16 +41,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         // Print documents directory to check the sqlite file
         print("Document Directory: \(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.path)")
-        
+
+        // Reset The Database
+//        resetDatabase()
+
+        // create an importer object to retrieve, parse, and import into the CoreData store
+        // pass the coordinator so the importer can create its own managed object context
         importer = iTunesJSONImporter(iTunesURL: iTunesURL, persistentStoreCoordinator: self.persistentContainer.persistentStoreCoordinator)
         importer?.delegate = self
         application.isNetworkActivityIndicatorVisible = true
 
+        // TODO add the importer to an operation queue for background processing (works on a separate thread)
+
         // Inject SongsViewController managedObjectContext.
+        // obtain our current initial view controller on the nav stack and set it's managed object context
         if let vc = window?.rootViewController as? UINavigationController, let songsViewController = vc.visibleViewController as? SongsViewController {
             songsViewController.managedObjectContext = self.persistentContainer.viewContext
         }
-        
+
         return true
     }
 
@@ -82,11 +96,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          error conditions that could cause the creation of the store to fail.
         */
         let container = NSPersistentContainer(name: "TopSongs")
+
+        if let url = self.persistentStorePath {
+            print("Changing persistentStorePath to \(url.path)")
+            container.persistentStoreDescriptions.first?.url = url
+        }
+
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -103,7 +123,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Core Data Saving support
 
-    func saveContext () {
+    func saveContext() {
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
@@ -115,6 +135,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
+    }
+
+    // remove the old store; easier than deleting every object
+    // first, test for an existing store
+    func resetDatabase() {
+
+        do {
+            if let url = self.persistentStorePath, FileManager.default.fileExists(atPath: url.path) {
+                try FileManager.default.removeItem(at: url)
+                print("Database file was deleted")
+            } else {
+                print("Could not find database file to delete")
+            }
+        } catch {
+            print("Error Resetting the database: \(error)")
+        }
+
     }
 
 }
